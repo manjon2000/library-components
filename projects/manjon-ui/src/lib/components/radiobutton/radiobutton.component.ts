1d0ef95648a3cf54bd2978a3d4b57df61e3d4967
Albert Manjon <albertmanjon123@gmail.com>
2024-11-26 22:31:18 +0100

    demo

--- projects/manjon-ui/src/lib/components/radiobutton/radiobutton.component.ts
+++ projects/manjon-ui/src/lib/components/radiobutton/radiobutton.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, forwardRef, Input, ViewChild, ViewEncapsulation } from '@angular/core';
 import { FocusBlurDirective } from '../../shared/directives/focus-blur.directive';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'ui-radiobutton',
@@ -9,13 +10,48 @@ import { FocusBlurDirective } from '../../shared/directives/focus-blur.directive
   ],
   templateUrl: './radiobutton.component.html',
   styleUrl: './radiobutton.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => UIRadiobuttonComponent),
+      multi: true
+    }
+  ],
 })
-export class UIRadiobuttonComponent {
+export class UIRadiobuttonComponent implements ControlValueAccessor {
+
+  @ViewChild('input', { static: true }) inputRadio!: ElementRef;
 
   @Input() label!: string;
   @Input() isChecked!: boolean;
   @Input() isDisabled!: boolean;
   @Input() isError!: boolean;
+
+  public onChangeCallback?: (checked: boolean) => void;
+  private _value!: boolean;
+
+  constructor(
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  onInput(event: Event): void {
+    const { checked } = (event.currentTarget) as HTMLInputElement
+   
+    this._value = checked;
+    this.onChangeCallback?.(checked);
+
+    this.cdr.markForCheck();
+  }
+
+  writeValue(checked: boolean): void {
+    this._value = checked;
+  }
+  registerOnChange(fn: any): void {
+    this.onChangeCallback = fn;
+  }
+  registerOnTouched(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
 
   public onFocus(): void {
     console.log('FOCUS!!!')
