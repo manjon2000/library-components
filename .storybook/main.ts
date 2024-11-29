4fac88ae8e7c8dce8a3dfce532237f988d2703e4
Albert Manjon <albertmanjon123@gmail.com>
2024-11-29 11:00:42 +0100

    ConfigurationGitHub Action for deploying Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,17 +3,21 @@ import * as path from "path";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
-    "@storybook/addon-styling-webpack"
+    "@storybook/addon-styling-webpack",
+    "@storybook/addon-mdx-gfm"
   ],
+
   framework: {
     name: "@storybook/angular",
     options: {},
   },
- 
+
+  docs: {}
 };
 export default config;
