1bf2e3d617568cd1f9ebc0c71c306fa111111671
Albert Manjon <albertmanjon123@gmail.com>
2024-11-29 13:46:28 +0100

    Remove all

--- projects/manjon-ui/src/lib/components/toggler/interface/toggler.interface.ts
+++
@@ -1,1 +0,0 @@
-export type TTogglerState =  'off' | 'on'
--- projects/manjon-ui/src/public-api.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of manjon-ui
- */
-
-// Radiobutton
-export * from './lib/components/radiobutton/radiobutton.component';
-
-//Toggler
-export * from './lib/components/toggler/toggler.component';
