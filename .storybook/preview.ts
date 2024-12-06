e4f7122b7fa509e26e90acd57078d189812abb1d
Albert Manjón <albertmanjon123@gmail.com>
2024-12-06 12:47:59 +0100

    Feature: Configuration rulesaddon-a11y

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,7 +3,7 @@ import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
 setCompodocJson(docJson);
 
-const preview: Preview = {
+const preview = {
   parameters: {
     controls: {
       matchers: {
@@ -12,8 +12,23 @@ const preview: Preview = {
       },
     },
   },
-
-  tags: ["autodocs"]
+  a11y: {
+    config: {
+      rules: [
+        { id: "color-contrast", enabled: true }, // Contraste de colores (WCAG 2.1 AA)
+        { id: "aria-roles", enabled: true }, // Roles ARIA
+        { id: "aria-required-children", enabled: true }, // Hijos requeridos por ARIA
+        { id: "aria-required-parent", enabled: true }, // Padres requeridos por ARIA
+        { id: "label", enabled: true }, // Etiquetas en controles interactivos
+        { id: "landmark-unique", enabled: true }, // Landmarks únicos
+        { id: "region", enabled: true }, // Uso correcto de regiones
+      ],
+    },
+    options: {
+      restoreScroll: true, // Soluciona problemas al analizar elementos fuera de pantalla
+    },
+  },
+  tags: ["autodocs"],
 };
 
 export default preview;
