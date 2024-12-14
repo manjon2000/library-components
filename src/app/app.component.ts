c5d106e2b8475893dc55934e63fd1215c742a5cc
Manjon2000 <albertmanjon123@gmail.com>
2024-12-14 13:14:57 +0100

    Fix: Fixed styles custom radiobutton WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UIRadiobuttonComponent } from '../../projects/manjon-ui/src/public-api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UIRadiobuttonComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'lib-ui';
